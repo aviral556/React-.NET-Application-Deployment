545a1f993f0ae505ffb9563e173262557ed22716
AVIRAL KHANDELWAL <aviral556@gmail.com>
2025-04-19 01:53:04 +0530

    Update iam-roles.tf

--- terraform/iam-roles.tf
+++ terraform/iam-roles.tf
@@ -1,1 +1,39 @@
+resource "aws_iam_role" "eks_node_role" {
+  name = "eks-node-role"
 
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_worker_node_ecr" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_worker_node_cloudwatch" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_worker_node_autoscaler" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_instance_profile" "eks_node_profile" {
+  name = "eks-node-instance-profile"
+  role = aws_iam_role.eks_node_role.name
+}
